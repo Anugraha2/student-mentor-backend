d84caf02240ac9ffa903f7a710d28d902d1765a0
SA Anugraha <78608126+Anugraha2@users.noreply.github.com>
2024-09-23 18:44:23 +0530

    Add files via upload

---
+++ src/routes/student.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import studentController from '../controller/student.js'
+
+const router = express.Router()
+
+router.get('/',studentController.getAllStudent)
+
+router.get('/:id',studentController.getStudent)
+
+router.post('/',studentController.addStudent)
+
+router.put('/:id/',studentController.editStudent)
+
+router.delete('/:id',studentController.deleteStudent)
+
+export default router
